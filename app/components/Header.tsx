720eb27c41b3eda5b953cf609ee4ccdc90b2fa25
Malav S Patel <malavpatel2607775@gmail.com>
2024-07-23 22:02:26 -0400

    added Header and Footer

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,21 +8,23 @@ const Header = () => {
   return (
     <div className="py-5 px-5 md:px-12 lg:px-28">
       <div className="flex justify-between items-center">
-        <span className="w-[130px] sm:w-auto">BLOGS</span>
+        <Link href="/" className="w-[130px] sm:w-auto">
+          BLOGS
+        </Link>
         <div className="text-center my-8">
           <h1 className="text-3xl sm:test-5xl font-medium">Latest Blogs</h1>
         </div>
         {login ? (
-          <Button className="flex items-center gap-2 font-medium py-1 px-3 ">
-            <Link href="/login">Login</Link>
-          </Button>
-        ) : (
           <div className="flex items-center">
             <Button variant="outline">Logout</Button>
             <Button variant="link">
               <Link href="/create">Create Post</Link>
             </Button>
           </div>
+        ) : (
+          <Button className="flex items-center gap-2 font-medium py-1 px-3 ">
+            <Link href="/login">Login</Link>
+          </Button>
         )}
       </div>
     </div>
