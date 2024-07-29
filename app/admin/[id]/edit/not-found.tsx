ce0dda0623307836234be6835b9b68d7ccf6d7f1
Malav S Patel <malavpatel2607775@gmail.com>
2024-07-28 22:11:31 -0400

    updated Not-Found Page

--- app/admin/[id]/edit/not-found.tsx
+++ app/admin/[id]/edit/not-found.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
-import { headers } from "next/headers";
 
 export default function NotFound() {
   return (
     <div className=" bg-blue-200  content-center w-auto h-screen flex items-center justify-center gap-2">
       <h2 className="text-xl font-semibold">404 Not Found</h2>
-      <p>Could not find the requested Page.</p>
+      <p>Could not find the requested blog.</p>
       <Link
-        href="/"
+        href="/admin"
         className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
       >
-        Return Home
+        Go Back
       </Link>
     </div>
   );
