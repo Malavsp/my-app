de18167a76ac20024073f186cf52f87340946f67
Malav S Patel <malavpatel2607775@gmail.com>
2024-07-28 22:09:59 -0400

    added Error Page

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
-const Error = () => {
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
   return (
     <>
       <section className="relative z-10 bg-primary py-[120px]">
@@ -16,24 +23,21 @@ const Error = () => {
                 <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
                   Oops! Something went wrong. Try again later
                 </h4>
-                <a
+                <button
+                  className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary mr-6"
+                  onClick={() => reset()}
+                >
+                  Try again
+                </button>
+                <Link
                   href="/"
                   className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary"
                 >
                   Go To Home
-                </a>
+                </Link>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-between space-x-5 md:space-x-8 lg:space-x-14">
-          <div className="h-full w-1/3 bg-gradient-to-t from-[#FFFFFF14] to-[#C4C4C400]"></div>
-          <div className="flex h-full w-1/3">
-            <div className="h-full w-1/2 bg-gradient-to-b from-[#FFFFFF14] to-[#C4C4C400]"></div>
-            <div className="h-full w-1/2 bg-gradient-to-t from-[#FFFFFF14] to-[#C4C4C400]"></div>
-          </div>
-          <div className="h-full w-1/3 bg-gradient-to-b from-[#FFFFFF14] to-[#C4C4C400]"></div>
         </div>
       </section>
     </>
